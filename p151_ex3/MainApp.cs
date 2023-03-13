3d69375ed80ec20809b354db67c7b88d03924e62
Ggab_Chuck <rlqja3605@gmail.com>
2023-03-13 15:32:44 +0900

    2023.03.13 : 문제 2번 풀이+

---
+++ p151_ex3/MainApp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace p151_ex3
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("숫자를 입력하세요. : ");
+
+            string a = Console.ReadLine();
+            int b = Int32.Parse(a);
+
+            Console.WriteLine( b >= 50 ? $"입력하신 숫자 {a}는(은) 50 이상 입니다." : $"입력하신 숫자 {a}는(은) 50 미만 입니다.");
+        }
+    }
+}

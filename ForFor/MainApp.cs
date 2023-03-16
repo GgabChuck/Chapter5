aefaddeca3636f9000ee697ca9cded1f21d0ba88
Ggab_Chuck <rlqja3605@gmail.com>
2023-03-16 15:42:06 +0900

    2023.03.16 : 시험 문제풀이 완

--- ForFor/MainApp.cs
+++ ForFor/MainApp.cs
@@ -5,7 +5,7 @@ namespace ForFor
     class MainApp
     {
         static void Main(string[] args)
-        {
+        {            
             for (int i = 0; i < 5; i++)
             {
                 for (int j = 0; j <= i; j++)

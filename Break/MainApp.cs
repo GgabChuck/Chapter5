e623e5e76f2200c3d1187b10295cef3f6c6c2582
Ggab_Chuck <rlqja3605@gmail.com>
2023-03-17 14:34:04 +0900

    2023.03.17 : Break 문 예제

---
+++ Break/MainApp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Break
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("계속할까요?(예/아니오) : ");
+                string a = Console.ReadLine();
+
+                if (a == "아니오")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}

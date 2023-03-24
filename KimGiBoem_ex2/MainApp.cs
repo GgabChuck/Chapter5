57f3345eee94643868ff06152df00bd6a83a19dd
Ggab_Chuck <rlqja3605@gmail.com>
2023-03-24 10:32:08 +0900

    2023.03.24 : 과제풀이 1,2

---
+++ KimGiBoem_ex2/MainApp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace KimGiBoem_ex2
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 4; i >= 0; i--)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

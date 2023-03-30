5b5ce3c2cd75212e36356f5845298cf6b9bf2156
Ggab_Chuck <rlqja3605@gmail.com>
2023-03-30 14:10:50 +0900

    문제 풀이중

---
+++ KimGiBoem_ex3/MainApp.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace KimGiBoem_ex3
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int a = 15;
+
+            do
+            {
+                Console.Write("*");
+                a -= 1;
+            }
+            while (a > 10);
+
+            Console.WriteLine("");
+
+            do
+            {
+                Console.Write("*");
+                a -= 1;
+            }
+            while (a > 6);
+
+            Console.WriteLine("");
+
+            do
+            {
+                Console.Write("*");
+                a -= 1;
+            }
+            while (a > 3);
+
+            Console.WriteLine("");
+
+            do
+            {
+                Console.Write("*");
+                a -= 1;
+            }
+            while (a > 1);
+
+            Console.WriteLine("");
+
+            do
+            {
+                Console.Write("*");
+                a -= 1;
+            }
+            while (a > 0);
+
+            Console.WriteLine("");
+        }
+    }
+}

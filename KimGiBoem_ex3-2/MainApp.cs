0b1c2d0cd827b57fe01de085e23f1a14f4abcdb7
Ggab_Chuck <rlqja3605@gmail.com>
2023-03-31 13:50:48 +0900

    2023.03.31 : 연습문제 풀이 완

--- KimGiBoem_ex3-2/MainApp.cs
+++ KimGiBoem_ex3-2/MainApp.cs
@@ -1,23 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace KimGiBoem_ex3
+namespace KimGiBoem_ex3_2
 {
     class MainApp
     {
         static void Main(string[] args)
         {
-            int a = 0;
-            while (a < 5)
+            int a = 5;
+
+            do
             {
                 int b = 0;
-                while (b <= a)
+                while (b < a)
                 {
                     Console.Write("*");
                     b++;
                 }
                 Console.WriteLine();
-                a++;
+                a--;
             }
+            while (a > 0);
         }
     }
 }
---
+++ KimGiBoem_ex4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace KimGiBoem_ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("반복 횟수를 입려하세요 : ");
+            int a = Int32.Parse(Console.ReadLine());
+
+            if (a <= 0)
+            {
+                Console.WriteLine("0보다 작거나 같은 수는 입력할 수 없습니다.");
+            }
+
+            for (int b = 0; b < a; b++)
+            {
+                for (int c = 0; c <= b; c++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
